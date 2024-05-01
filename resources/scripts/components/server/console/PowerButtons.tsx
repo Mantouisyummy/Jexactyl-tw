6b744930e339dd0c1c516ea1b8326033cb7ed325
Mantou <opcantel@gmail.com>
2024-05-01 21:38:18 +0800

    translate: power button

--- resources/scripts/components/server/console/PowerButtons.tsx
+++ resources/scripts/components/server/console/PowerButtons.tsx
@@ -42,11 +42,11 @@ export default ({ className }: PowerButtonProps) => {
                 open={open}
                 hideCloseIcon
                 onClose={() => setOpen(false)}
-                title={'Forcibly Stop Process'}
-                confirm={'Continue'}
+                title={'強制停止進程'}
+                confirm={'繼續'}
                 onConfirmed={onButtonClick.bind(this, 'kill-confirmed')}
             >
-                Forcibly stopping a server can lead to data corruption.
+                強制停止伺服器可能導致資料毀損.
             </Dialog.Confirm>
             <Can action={'control.start'}>
                 <Button.Success
@@ -54,12 +54,12 @@ export default ({ className }: PowerButtonProps) => {
                     disabled={status !== 'offline'}
                     onClick={onButtonClick.bind(this, 'start')}
                 >
-                    Start
+                    啟動
                 </Button.Success>
             </Can>
             <Can action={'control.restart'}>
                 <Button.Text className={'flex-1'} disabled={!status} onClick={onButtonClick.bind(this, 'restart')}>
-                    Restart
+                    重啟
                 </Button.Text>
             </Can>
             <Can action={'control.stop'}>
@@ -68,7 +68,7 @@ export default ({ className }: PowerButtonProps) => {
                     disabled={status === 'offline'}
                     onClick={onButtonClick.bind(this, killable ? 'kill' : 'stop')}
                 >
-                    {killable ? 'Kill' : 'Stop'}
+                    {killable ? '強制停止' : '停止'}
                 </Button.Danger>
             </Can>
         </div>
